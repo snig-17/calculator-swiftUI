59c812cae27722fe173407aa8a5b11ae765320a5
snig-17 <snigdha.17.tiwari@icloud.com>
2025-08-31 22:24:02 +0530

    Initial Commit

---
+++ calculator-swift/calculator_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculator_swiftApp.swift
+//  calculator-swift
+//
+//  Created by Snigdha Tiwari  on 31/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct calculator_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ calculator-swiftTests/calculator_swiftTests.swift
@@ -0,0 +1,16 @@
+//
+//  calculator_swiftTests.swift
+//  calculator-swiftTests
+//
+//  Created by Snigdha Tiwari  on 31/08/2025.
+//
+
+import Testing
+
+struct calculator_swiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
